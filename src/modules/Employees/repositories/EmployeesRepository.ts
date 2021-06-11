43a0e682a0d9d791318ce739a0b39595b0209d38
Victor Campos <campos.viictor@gmail.com>
2021-06-11 16:05:47 -0300

    feat(repositories): add Employees implementation

---
+++ src/modules/Employees/repositories/EmployeesRepository.ts
@@ -0,0 +1,74 @@
+import { Employee } from "../model/Employee";
+import {
+  ICreateEmployeeDTO,
+  IEmployeesRepository,
+  IUpdateEmployeeDTO,
+} from "./IEmployeesRepository";
+
+export class EmployeesRepository implements IEmployeesRepository {
+  private employees: Employee[];
+
+  private static INSTANCE: EmployeesRepository;
+
+  private constructor() {
+    this.employees = [];
+  }
+
+  public static getInstance(): EmployeesRepository {
+    if (!EmployeesRepository.INSTANCE) {
+      EmployeesRepository.INSTANCE = new EmployeesRepository();
+    }
+
+    return EmployeesRepository.INSTANCE;
+  }
+
+  list(): Employee[] {
+    return this.employees;
+  }
+
+  findById(id: string): Employee {
+    return this.employees.find((employee) => employee.id === id);
+  }
+
+  findByCpf(cpf: string): Employee {
+    const employee = this.employees.find((employee) => employee.cpf === cpf);
+
+    return employee;
+  }
+
+  findByEmail(email: string): Employee {
+    const employee = this.employees.find(
+      (employee) => employee.email === email
+    );
+
+    return employee;
+  }
+
+  create(employee: ICreateEmployeeDTO): Employee {
+    const newEmployee = new Employee();
+
+    Object.assign(newEmployee, {
+      ...employee,
+    });
+
+    this.employees.push(newEmployee);
+
+    return newEmployee;
+  }
+
+  edit(id: string, updatedData: IUpdateEmployeeDTO): Employee {
+    const editedEmployee: Employee = {
+      id,
+      ...updatedData,
+    };
+
+    this.employees = this.employees.filter((employee) => employee.id !== id);
+    this.employees.push(editedEmployee);
+
+    return editedEmployee;
+  }
+
+  remove(id: string): void {
+    this.employees = this.employees.filter((employee) => employee.id !== id);
+  }
+}
