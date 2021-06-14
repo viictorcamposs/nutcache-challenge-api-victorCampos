1a3b0e9f051f12f7d35186e08351f6c96fa85a96
Victor Campos <campos.viictor@gmail.com>
2021-06-14 16:17:30 -0300

    feat: add middleware to handle http errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 
+import "express-async-errors";
+
+import { AppError } from "./errors/AppError";
 import { employeesRoutes } from "./routes/employees.routes";
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+
 app.use("/nutemployee", employeesRoutes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json(err.message);
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3030, () => console.log("Server is running on port 3030"));
